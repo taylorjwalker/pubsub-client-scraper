783ea915b5aa5fd34870d8a6e117a554fc92b5a6
Taylor Walker <taylorjameswalker@gmail.com>
2018-05-17 04:22:47 -0400

    Trying to get this to work... again

--- func/pubsub.js
+++ func/pubsub.js
@@ -2,12 +2,20 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 
 exports.handler = function(event, context, callback) {
-  const storeID = "2622294";
-  const corsProxy = "https://crossorigin.me/";
-  const baseUrl = `http://weeklyad.publix.com/Publix/BrowseByListing/BySearch/?StoreID=${storeID}&SearchText=whole+sub`;
-  const url = baseUrl; // `${corsProxy}${baseUrl}`;
+  const respond = ({ status, body }) => {
+    callback(null, {
+      statusCode: status,
+      headers: {
+        "Access-Control-Allow-Origin": "*"
+      },
+      body: JSON.stringify({ body })
+    });
+  };
 
-  nextBlankday = dayOfWeek => {
+  const storeID = "2622294";
+  const url = `http://weeklyad.publix.com/Publix/BrowseByListing/BySearch/?StoreID=${storeID}&SearchText=whole+sub`;
+
+  const nextBlankday = dayOfWeek => {
     const now = new Date();
     const result = new Date(
       now.getFullYear(),
@@ -18,54 +26,28 @@ exports.handler = function(event, context, callback) {
     return result;
   };
 
-  // const handleErrors = response => {
-  //   if (!response.ok) throw Error(response.statusText);
-  //   return response;
-  // };
-
-  // const scrapeData = html => {
-  //   const sub = Array.from(
-  //     html.documentElement.querySelectorAll("span.desktopBBDTabletTitle")
-  //   ).filter(title => title.innerHTML.toLowerCase().includes("sub"))[0];
-  //   const name = sub.innerText.trim();
-  //   const price = sub.parentNode.nextElementSibling.innerText.trim();
-  //   const nextThursday = new Date(`nextBlankday`(4)).getTime();
-  //   const pubsub = { name: name, price: price, expires: nextThursday };
-  //   return pubsub;
-  // };
-
-  const getPubsub = async url => {
-    axios.get(url).then(
-      response => {
-        if (response.status === 200) {
-          const html = response.data;
-          const $ = cheerio.load(html);
-          const sub = $("span.desktopBBDTabletTitle").filter((i, el) =>
-            $(this)
-              .text()
-              .toLowerCase()
-              .includes("sub")
-          )[0];
-          const name = sub.text().trim();
-          const price = sub
-            .parent()
-            .next()
-            .text()
-            .trim();
-          const nextThursday = new Date(`nextBlankday`(4)).getTime();
-          const pubsub = { name: name, price: price, expires: nextThursday };
-          return pubsub;
-        }
-      },
-      error => console.log(err)
-    );
-  };
-
-  callback(null, {
-    statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "*"
-    },
-    body: getPubsub(url)
-  });
+  axios
+    .get(url)
+    .then(response => {
+      if (response.status === 200) {
+        const html = response.data;
+        const $ = cheerio.load(html);
+        const sub = $("span.desktopBBDTabletTitle:contains('Whole Sub')");
+        const name = sub.text().trim();
+        const price = sub
+          .parent()
+          .next()
+          .text()
+          .trim();
+        const nextThursday = new Date(nextBlankday(4)).getTime();
+        const pubsub = { name: name, price: price, expires: nextThursday };
+        return pubsub;
+      }
+    })
+    .then(pubsub => {
+      respond({ status: 200, body: pubsub });
+    })
+    .catch(err => {
+      respond({ status: 422, body: "Couldn't get the data" });
+    });
 };
--- functions/pubsub.js
+++ functions/pubsub.js
@@ -16325,73 +16325,45 @@ module.exports = flatten;
 "use strict";
 
 
-function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
-
 const axios = __webpack_require__(154);
 const cheerio = __webpack_require__(183);
 
 exports.handler = function (event, context, callback) {
-  var _this = this;
+  const respond = ({ status, body }) => {
+    callback(null, {
+      statusCode: status,
+      headers: {
+        "Access-Control-Allow-Origin": "*"
+      },
+      body: JSON.stringify({ body })
+    });
+  };
 
   const storeID = "2622294";
-  const corsProxy = "https://crossorigin.me/";
-  const baseUrl = `http://weeklyad.publix.com/Publix/BrowseByListing/BySearch/?StoreID=${storeID}&SearchText=whole+sub`;
-  const url = baseUrl; // `${corsProxy}${baseUrl}`;
+  const url = `http://weeklyad.publix.com/Publix/BrowseByListing/BySearch/?StoreID=${storeID}&SearchText=whole+sub`;
 
-  nextBlankday = dayOfWeek => {
+  const nextBlankday = dayOfWeek => {
     const now = new Date();
     const result = new Date(now.getFullYear(), now.getMonth(), now.getDate() + (7 + dayOfWeek - now.getDay()) % 7);
     if (result < now) result.setDate(result.getDate() + 7);
     return result;
   };
 
-  // const handleErrors = response => {
-  //   if (!response.ok) throw Error(response.statusText);
-  //   return response;
-  // };
-
-  // const scrapeData = html => {
-  //   const sub = Array.from(
-  //     html.documentElement.querySelectorAll("span.desktopBBDTabletTitle")
-  //   ).filter(title => title.innerHTML.toLowerCase().includes("sub"))[0];
-  //   const name = sub.innerText.trim();
-  //   const price = sub.parentNode.nextElementSibling.innerText.trim();
-  //   const nextThursday = new Date(`nextBlankday`(4)).getTime();
-  //   const pubsub = { name: name, price: price, expires: nextThursday };
-  //   return pubsub;
-  // };
-
-  const getPubsub = (() => {
-    var _ref = _asyncToGenerator(function* (url) {
-      axios.get(url).then(function (response) {
-        if (response.status === 200) {
-          const html = response.data;
-          const $ = cheerio.load(html);
-          const sub = $("span.desktopBBDTabletTitle").filter(function (i, el) {
-            return $(_this).text().toLowerCase().includes("sub");
-          })[0];
-          const name = sub.text().trim();
-          const price = sub.parent().next().text().trim();
-          const nextThursday = new Date(`nextBlankday`(4)).getTime();
-          const pubsub = { name: name, price: price, expires: nextThursday };
-          return pubsub;
-        }
-      }, function (error) {
-        return console.log(err);
-      });
-    });
-
-    return function getPubsub(_x) {
-      return _ref.apply(this, arguments);
-    };
-  })();
-
-  callback(null, {
-    statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "*"
-    },
-    body: getPubsub(url)
+  axios.get(url).then(response => {
+    if (response.status === 200) {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      const sub = $("span.desktopBBDTabletTitle:contains('Whole Sub')");
+      const name = sub.text().trim();
+      const price = sub.parent().next().text().trim();
+      const nextThursday = new Date(nextBlankday(4)).getTime();
+      const pubsub = { name: name, price: price, expires: nextThursday };
+      return pubsub;
+    }
+  }).then(pubsub => {
+    respond({ status: 200, body: pubsub });
+  }).catch(err => {
+    respond({ status: 422, body: "Couldn't get the data" });
   });
 };
 
